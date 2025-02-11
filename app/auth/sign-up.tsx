c4daededc9d6f2afb4473349efb33125f5994d01
Sunay Shehaan <sunayshehaan@Sunays-Laptop.local>
2025-02-11 14:14:22 -0600

    done sign up

--- app/auth/sign-up.tsx
+++ app/auth/sign-up.tsx
@@ -1,7 +1,27 @@
 import React from "react";
 
+import { getAuth, createUserWithEmailAndPassword, sendPasswordResetEmail } from "firebase/auth";
+
 const SignUp = () => {
+  const auth = getAuth()
+  const email = ''
+  const password = ''
+  createUserWithEmailAndPassword(auth, "testuser1@gmail.com", "Hello123he")
+  .then(()=>{
+    console.log("User created successfully!")
+})
+  .catch((error)=>{
+    const errorCode = error.code;
+                if (errorCode === 'auth/email-already-in-use') {
+                    console.log("Email already exists!");
+                }
+  })
+
+
+
+
   return <div>SignUp</div>;
 };
+
 
 export default SignUp;
