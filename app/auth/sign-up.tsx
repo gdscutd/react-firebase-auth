ab4f577ac74c6298e2fa379a830cedf9823fcc95
awesomness560 <appsforyou6@gmail.com>
2025-02-11 18:01:35 -0600

    Started adding ui

--- app/auth/sign-up.tsx
+++ app/auth/sign-up.tsx
@@ -1,27 +1,69 @@
-import React from "react";
+"use client";
+import { Button } from "~/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "~/components/ui/form";
+import { Input } from "~/components/ui/input";
 
-import { getAuth, createUserWithEmailAndPassword, sendPasswordResetEmail } from "firebase/auth";
+//import { getAuth, createUserWithEmailAndPassword, sendPasswordResetEmail } from "firebase/auth";
+
+import { z } from "zod";
+
+const formSchema = z.object({
+  username: z.string().min(2).max(50),
+});
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
 
 const SignUp = () => {
-  const auth = getAuth()
-  const email = ''
-  const password = ''
-  createUserWithEmailAndPassword(auth, "testuser1@gmail.com", "Hello123he")
-  .then(()=>{
-    console.log("User created successfully!")
-})
-  .catch((error)=>{
-    const errorCode = error.code;
-                if (errorCode === 'auth/email-already-in-use') {
-                    console.log("Email already exists!");
-                }
-  })
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
+    },
+  });
 
+  // 2. Define a submit handler.
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values);
+  }
 
-
-
-  return <div>SignUp</div>;
+  return (
+    <div className="bg-black flex h-screen justify-center items-center">
+      <div className="xl:w-1/3 lg:w-1/2 md:w-1/2 sm:w-3/4 w-4/5">
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+            <FormField
+              control={form.control}
+              name="username"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Username</FormLabel>
+                  <FormControl>
+                    <Input placeholder="shadcn" {...field} />
+                  </FormControl>
+                  <FormDescription>
+                    This is your public display name.
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button type="submit">Submit</Button>
+          </form>
+        </Form>
+      </div>
+    </div>
+  );
 };
-
 
 export default SignUp;
