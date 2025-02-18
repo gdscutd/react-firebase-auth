55b86bfcae4af30dd43e9c6a1e91eb170b985615
GDSC UTD <dscutd@gmail.com>
2025-02-17 22:37:23 -0600

    Update config.ts

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -6,11 +6,11 @@ const apiKey = import.meta.env.VITE_FIREBASE_KEY;
 
 export const firebaseConfig = {
   apiKey: apiKey,
-  authDomain: "gdsc-workshop-6507d.firebaseapp.com",
-  projectId: "gdsc-workshop-6507d",
-  storageBucket: "gdsc-workshop-6507d.firebasestorage.app",
-  messagingSenderId: "433139504580",
-  appId: "1:433139504580:web:2e2521eccdd62a226f4091",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
 };
 
 const firebase = initializeApp(firebaseConfig);
